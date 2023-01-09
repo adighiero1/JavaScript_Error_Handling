e43c9c4e41023190a745563e72dee7638e09fe95
Alejandro Dighiero <adighiero1@gmail.com>
2023-01-09 15:39:18 -0500

    Alejandro Excercise

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 // TODO: Enable strict mode
-
+"use strict";
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
   // TODO: if an exception is raised
   // print the error to the console
   // and return null
 }
 
-let failData = "<h1>Hello World!</h1>"
+let failData = "<h1>Hello World!</h1>";
 
 let passData = JSON.stringify({ success: true });
 
-console.log(parseToJSON(failData));
+console.log(parseToJSON(failData)); // we expect to see null
 
-console.log(parseToJSON(passData));
+console.log(parseToJSON(passData)); // we will see an object {success:true}
